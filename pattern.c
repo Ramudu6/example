7dce20fd915be1ed2365620fd404a8482550ae6e
sainarayana9989 <sainarayana9989@gmail.com>
2023-11-28 22:51:58 +0530

    added file c

---
+++ pattern.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+  int i,j,space,k=o,n;
+  printf("\nenter the number of rows : ");
+  scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+  { 
+    for(int j=1;j<=n-1;j++)
+     { 
+       printf(" ");
+     }
+     for(j=1;j<=1;j++)
+     {
+       printf("*");
+     }
+     printf("\n");
+   }
+  for(int i=n-1;i>0;i-)
+   {
+     for(int j=1;j<=n-i;j++)
+     {
+       printf(" ");
+      }
+     for(int j=1;j<=i;j++);
+     {
+       printf("*");
+      }
+      printf("\n");
+    }
+}
